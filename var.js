0565772321abd251c829f82b244499bae7a12aec
ukana gabi dimitri <gabidimitri0103@gmail.com>
2026-07-23 08:13:13 +0700

    var.js

---
+++ var.js
@@ -0,0 +1,14 @@
+//declarasi variabel
+let angka = 10;          //number
+let sekolah = "moklet";  //string
+let kondisi = false;     //boolean
+let data;                //undefined
+let nama = null;         //null
+
+console.log("typeof angka): " + typeof angka);
+console.log("typeof sekolah): " + typeof sekolah);
+console.log("typeof kondisi): " + typeof kondisi);
+console.log("typeof data): " + typeof data);
+console.log("typeof nama): " + typeof nama);
+
+console.log(`ini adalah angka  ${angka}`);
